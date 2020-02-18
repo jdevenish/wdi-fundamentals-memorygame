73c0e38147f20d8d4864bf3c92321f9030894940
jdevenish <justin.devenish@gmail.com>
2020-02-17 20:59:34 -0500

    Add reset and edge case logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,16 +27,7 @@ let cards = [
 
 cardsInPlay = [];
 score = {
-	player : 0,
-	computer: 0
-}
-
-/*
- * Helper function that prints card image path and suit
- */
-function printCardObj(cardObj) {
-	console.log(cardObj.cardImage);
-	console.log(cardObj.suit);
+	player : 0
 }
 
 /*
@@ -47,35 +38,102 @@ function printCardObj(cardObj) {
 function checkForMatch(){
 	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		score.player += 1;
-	  	alert("You found a match! \n "+
-	  		"Player: " + score.player + "\n" +
-	  		"Computer: " + score.computer);
+
+	  	// alert("You found a match! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer);
+
+	  	// setTimeout(function(){ alert("You found a match! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer); }, 750);
+
+	  	// confirm("You found a match! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer);
+
+	  	createResultsBoard(true);
 	} else {
-		score.computer += 1;
-	  	alert("Sorry, try again! \n "+
-	  		"Player: " + score.player + "\n" +
-	  		"Computer: " + score.computer);
+	  	// alert("Sorry, try again! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer);
+
+	  	// setTimeout(function(){ alert("Sorry, try again! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer); }, 750);
+
+	  	// confirm("Sorry, try again! \n "+
+	  	// 	"Player: " + score.player + "\n" +
+	  	// 	"Computer: " + score.computer);
+
+	  	createResultsBoard(false);
 	}
-	resetBoard();
-	cardsInPlay = [];
 }
+
 
 /*
  * Flip card to selected state
  */
 function flipCard() {
 	let cardId = this.getAttribute("data-id");
-	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length > 1){
-		checkForMatch();
+	switch (cardsInPlay.length) {
+		case 0: // No cards, get to playing!
+			break; 
+		case 1: // One card, keep going
+			this.setAttribute("src", cards[cardId].cardImage);
+			break;
+		case 2: // Two cards, let's see how we did
+			if (cardsInPlay[0] !== cardsInPlay[1]){ // Don't pick the same card
+				this.setAttribute("src", cards[cardId].cardImage);
+				checkForMatch();
+			} else{
+				cardsInPlay.pop();
+			}
+			break;
+		default: // Ignore the click
+			break;
 	}
+	
 }
+
+/*
+ * Create Results Board
+ */
+function createResultsBoard(successValue) {
+	const resultsBoard = document.getElementById("result-board");
+	resultsBoard.classList.add("results");
+
+	// Create outcome label
+	const resultMessage = document.createElement('h2');
+	resultMessage.classList.add("bannerHeading");
+	if (successValue){
+		resultMessage.textContent = "Match Found!";
+	} else {
+		resultMessage.textContent = "Sorry, Try Again!";
+	}
+	
+	// Create scoreboard
+	const resultTable = document.createElement('p');
+	resultTable.classList.add("bannerHeading")
+	resultTable.textContent = "Score: " + score.player;
+
+	// Create button
+	const acceptButton = document.createElement('Button');
+	acceptButton.textContent = "Accept";
+	acceptButton.addEventListener("click", resetBoard);
+
+	// Build elements up
+	resultsBoard.appendChild(resultMessage);
+	resultsBoard.appendChild(resultTable);
+	resultsBoard.appendChild(acceptButton);
+}
+
 
 /*
  * Create a new board
  */
 function createBoard() {
+	cards = shuffle(cards);
 	for(let i=0; i<cards.length; i++){
 		const board = document.getElementById("game-board");
 		let tempCard = document.createElement('img');
@@ -90,12 +148,47 @@ function createBoard() {
  * Remove and rebuild the board
  */
 function resetBoard() {
+	// Reset Cards
 	for(let i=0; i<cards.length; i++){
 		const board = document.getElementById("game-board");
 		board.removeChild(board.childNodes[0]);
 	}
+
+	// Reset Result Board
+	const results = document.getElementById("result-board");
+	while (results.firstChild) {
+	    results.removeChild(results.lastChild);
+	}
+	results.classList.remove("results");
+
+	// Reset cardsInPlay tracker
+	cardsInPlay = [];
+
+	// Reset board
 	createBoard();
 }
 
+/*
+ * shuffle cards
+ * https://github.com/Daplie/knuth-shuffle
+ */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 createBoard();
